be8b15f01522c5bff9afde437accfa89c627b6d7
pawel.koselski <pawel.koselski@aptitudesoftware.com>
2019-01-19 10:39:46 +0100

    Task 2 - ADDITIONAL task completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'devmeetings-angular';
-  public products: Array<{id: number, name: string}> = [
-    {id: 1, name: 'Prod 1'},
-    {id: 2, name: 'Prod 2'},
-    {id: 3, name: 'Prod 3'},
-    {id: 4, name: 'Prod 4'},
-    {id: 5, name: 'Prod 5'},
+  public products: Array<{id: number, name: string, isPromoted: boolean}> = [
+    {id: 1, name: 'Prod 1', isPromoted: true},
+    {id: 2, name: 'Prod 2', isPromoted: false},
+    {id: 3, name: 'Prod 3', isPromoted: true},
+    {id: 4, name: 'Prod 4', isPromoted: false},
+    {id: 5, name: 'Prod 5', isPromoted: true},
   ];
 }
